4680673b8a72d89779a0d6860fd70b1c049aa6b0
Ruben Paz <me@ruben.io>
2019-10-05 18:03:34 +0100

    Use connection string for db connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -19,11 +19,8 @@ async function getEnv(key: string): Promise<string> {
 export async function getConnection(): Promise<Pool> {
   if (connection.isEmpty()) {
     logger.info("Creating a new pool of database clients");
-    const host = await getEnv("DB_HOST");
-    const database = await getEnv("DB_NAME");
-    const user = await getEnv("DB_USER");
-    const password = await getEnv("DB_PASS");
-    const pool = new Pool({ host, database, user, password });
+    const connectionString = await getEnv("DATABASE_URL");
+    const pool = new Pool({ connectionString });
     pool.on("error", e => {
       logger.error("Error on database connection", e);
       // TODO Connection retry instead of exit
